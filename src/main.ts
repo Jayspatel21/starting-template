b0407639d96269c44025496e062aa964985af8a8
Ravindra Valand <ravindra@linearloop.io>
2024-10-17 15:45:27 +0530

    new changes

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const PORT = process.env.PORT || 3000;
   const ENV = process.env.NODE_ENV || 'development';
   await app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT} +  le laie lee`);
     console.log(`Environment: ${ENV}`);
   });
 }
