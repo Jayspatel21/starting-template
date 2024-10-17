2f7c38d507cfd79e488ad7b4f7acf6403eade1d6
Ravindra Valand <ravindra@linearloop.io>
2024-10-17 15:43:42 +0530

    chanagas

--- dist/main.js
+++ dist/main.js
@@ -4,7 +4,12 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(3000);
+    const PORT = process.env.PORT || 3000;
+    const ENV = process.env.NODE_ENV || 'development';
+    await app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+        console.log(`Environment: ${ENV}`);
+    });
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
